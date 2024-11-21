59e2fbb334c6417cb573e5d44d4e0a3718c4de37
KVBAMusic <kubaokon8@gmail.com>
2024-11-21 09:38:17 +0100

    Cw4_2be

--- index.js
+++ index.js
@@ -45,7 +45,53 @@ app.get('/math/power/:base/:exponent', (req, res) => {
 });
 
 //TODO2
+let categories = ['funnyJoke', 'lameJoke'];
+let funnyJoke = [
+  {
+    'joke': 'Dlaczego komputer poszedł do lekarza?',
+    'response': 'Bo złapał wirusa!'
+  },
+  {
+    'joke': 'Dlaczego komputer nie może być głodny?',
+    'response': 'Bo ma pełen dysk!'
+  },
+  {
+    'joke': 'Co mówi jeden bit do drugiego?',
+    'response': '„Trzymaj się, zaraz się przestawiamy!”'
+  }
+];
+let lameJoke = [
+  {
+    'joke': 'Dlaczego programiści preferują noc?',
+    'response': 'Bo w nocy jest mniej bugów do łapania!'
+  },
+  {
+    'joke': 'Jak nazywa się bardzo szybki programista?',
+    'response': 'Błyskawiczny kompilator!'
+  }
+];
 
+
+app.get("/jokebook/categories", (req, res) => {
+  res.json(categories)
+})
+
+app.get("/jokebook/joke/:category", (req, res) => {
+  const category = req.params.category
+  let reply
+  switch (category) {
+    case "funnyJoke":
+      reply = funnyJoke[Math.floor(Math.random() * funnyJoke.length)]
+      break
+    case "lameJoke":
+      reply = lameJoke[Math.floor(Math.random() * lameJoke.length)]
+      break
+    default:
+      reply = { "error": `no jokes for category ${category}` }
+      break
+  }
+  res.json(reply)
+})
 
 //TODO3
 
